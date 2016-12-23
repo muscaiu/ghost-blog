99165fc9cb7016b2454c764ef1f5d2902b5fc68e
muscaiu <muscaiu@yahoo.com>
2016-12-23 13:40:52 +0200

    changed all ports to 2368

--- config.js
+++ config.js
@@ -1,17 +1,10 @@
-// # Ghost Configuration
-// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
-
-// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
 
 var path = require('path'),
     config;
 
 config = {
-    // ### Production
-    // When running Ghost in the wild, use the production environment.
-    // Configure your URL and mail settings here
     production: {
-        url: 'http://cristian.muscalu.net',
+        url: 'http://127.0.0.1:2368',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -23,38 +16,11 @@ config = {
 
         server: {
             host: '127.0.0.1',
-            port: '8080'
+            port: '2368'
         }
     },
-
-    // ### Development **(default)**
     development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blog's published URL.
-        url: 'http://localhost:2368',
-
-        // Example refferer policy
-        // Visit https://www.w3.org/TR/referrer-policy/ for instructions
-        // default 'origin-when-cross-origin',
-        // referrerPolicy: 'origin-when-cross-origin',
-
-        // Example mail config
-        // Visit http://support.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
-
-        // #### Database
-        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
+        url: 'http://127.0.0.1:2368',
         database: {
             client: 'sqlite3',
             connection: {
@@ -62,26 +28,14 @@ config = {
             },
             debug: false
         },
-        // #### Server
-        // Can be host & port (default), or socket
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+           port: '2368'
         },
-        // #### Paths
-        // Specify where your content directory lives
         paths: {
             contentPath: path.join(__dirname, '/content/')
         }
     },
-
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
     testing: {
         url: 'http://127.0.0.1:2369',
         database: {
@@ -105,9 +59,6 @@ config = {
         },
         logging: false
     },
-
-    // ### Testing MySQL
-    // Used by Travis - Automated testing run through GitHub
     'testing-mysql': {
         url: 'http://127.0.0.1:2369',
         database: {
